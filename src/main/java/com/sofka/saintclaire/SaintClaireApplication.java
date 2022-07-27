6633f84d16e222f695085d246fb293261a217850
Santiago Sierra <santiagosierrak10@gmail.com>
2022-07-27 17:16:05 -0500

    Initial commit

---
+++ src/main/java/com/sofka/saintclaire/SaintClaireApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.saintclaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintClaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintClaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/saintclaire/SaintclaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.saintclaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintclaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
