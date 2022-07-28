d0980a83f7be0041b394f8b4deeaa9686c5a746f
Santiago Sierra <santiagosierrak10@gmail.com>
2022-07-27 19:01:40 -0500

    Basic Structure. Model completed. DB Validations added

---
+++ src/main/java/com/sofka/saintclaire/model/Patient.java
@@ -0,0 +1,42 @@
+package com.sofka.saintclaire.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Entity(name = "Patient")
+@Table(name = "patient")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @SequenceGenerator(
+            name = "patient_sequence",
+            sequenceName = "patient_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "patient_sequence"
+    )
+    private Long patientId;
+    @Column(updatable = false)
+    @Size(min = 10, max = 45, message
+            = "Patient Name must be between 10 and 45 characters")
+    private String patientName;
+    @Column(updatable = false)
+    @Min(1)
+    private int age;
+    private String appointmentDates;
+    private int numberOfAppointments;
+    private Long fkSpecialtyId;
+
+
+
+}
---
+++ src/main/java/com/sofka/saintclaire/model/Specialty.java
@@ -0,0 +1,46 @@
+package com.sofka.saintclaire.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity(name = "Specialty")
+@Table(name = "specialty")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Specialty {
+
+    @Id
+    @SequenceGenerator(
+            name = "specialty_sequence",
+            sequenceName = "specialty_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "specialty_sequence"
+    )
+    private Long specialtyId;
+    @Size(min = 5, max = 100, message
+            = "Specialty Name must be between 5 and 100 characters")
+    private String specialtyName;
+    @Size(min = 10, max = 45, message
+            = "Physician Name must be between 10 and 45 characters")
+    private String physicianInCharge;
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private Set<Patient> patientList = new HashSet<>();
+
+
+
+
+}
